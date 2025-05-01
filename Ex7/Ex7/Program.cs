02a28d8b74aa032b6bebc4c02f99d44df5c11fa6
allanacristinag <allana.g@aluno.ifsp.edu.br>
2025-04-30 22:14:22 -0300

    Add files via upload

---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int milha;
+            int kmMetros;
+            int kmMilhas;
+
+            milha = 1852;
+            kmMetros = 1000;
+            kmMilhas = milha / kmMetros;
+
+            Console.Write("")
+            
+        }
+    }
+}
